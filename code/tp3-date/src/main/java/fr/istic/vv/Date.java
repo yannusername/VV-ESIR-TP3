753c717b6e40671dadc2cc8d7277947876877ccf
baptistor <baptk35@gmail.com>
2024-10-11 13:01:51 +0200

    ex date

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -1,17 +1,204 @@
 package fr.istic.vv;
 
 class Date implements Comparable<Date> {
+    private int day;
+    private int month;
+    private int year;
 
-    public Date(int day, int month, int year) { }
+    public Date(int day, int month, int year) {
+        if (isValidDate(day, month, year)){
+            this.day=day;
+            this.month=month;
+            this.year=year;
+        }
+        else{
+            throw new IllegalArgumentException("Format de date invalide");
+        }
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
 
-    public static boolean isLeapYear(int year) { return false; }
+    }
 
-    public Date nextDate() { return null; }
+    public static boolean isValidDate(int day, int month, int year) {
+        if (day<1 || day>31 ){ // Cas Jour, Mois ou Année négative + Mois inférieur à 13 + Jour supérieur à 31
+            return false;
+        }
+        if (month<1 || month > 12){
+            return false;
+        }
+        int[] month_30 = {4, 6, 9, 11};
+        for (int elem : month_30){
+            if (elem==month){ // 30 Jours dans le Mois de Avril, Juin, Septembre, Novembre
+                if (day>30){
+                    return false;
+                }
+            }
+        }
 
-    public Date previousDate() { return null; }
+        if (month==2){ // Cas du mois de février en prenant en compte les années bisextilles 
+            if (year%4 ==0){
+                if (day>29){
+                    return false;
+                }
+            }
+            else{
+                if (day>28){
+                    return false;
+                }
+            } 
+        }
 
-    public int compareTo(Date other) { return 0; }
+        return true;
+    }
+
+    public static boolean isLeapYear(int year) { return year%4==0;}
+
+    public Date nextDate() {
+        int[] month_30 = {4, 6, 9, 11};
+        int[] month_31 = {1, 3, 5, 7, 8, 10, 12};
+        for (int elem: month_30){ // POur les mois de 30 jours
+            if(elem==this.month){
+                if (this.day==30){
+                    return new Date(1, this.month+1, this.year);
+                }
+                else{
+                    return new Date(this.day+1, this.month, this.year);
+                }
+            }
+        }
+
+        for (int elem: month_31){ // POur les mois de 31 jours
+            if(elem==this.month){
+                if (this.day==31 && this.month==12){ // Cas du 31 Décembre
+                    return new Date(1, 1, this.year+1);
+                }
+                else if (this.day==31){
+                    return new Date(1, this.month+1, this.year);
+                }
+                else{
+                    return new Date(this.day+1, this.month, this.year);
+                }
+            }
+        }
+
+            // Cas du mois de février en prenant en compte les années bisextilles 
+        if (this.year%4 ==0){
+            if (day==29){
+                return new Date(1, this.month+1, this.year);
+            }
+            else{
+                return new Date(this.day+1, this.month, this.year);
+            }
+        }
+
+        if (day==28){
+            return new Date(1, this.month+1, this.year);
+        }
+        
+        return new Date(this.day+1, this.month, this.year);
+    }
+
+    public Date previousDate() {
+        int[] month_30 = {4, 6, 9, 11};
+        int[] month_31 = {1, 3, 5, 7, 8, 10, 12};
+
+        for (int elem: month_30){ // POur les mois de 30 jours
+            if(elem==this.month){
+                if (this.day==1){
+                    return new Date(31, this.month-1, this.year);
+                }
+                else{
+                    return new Date(this.day-1, this.month, this.year);
+                }
+            }
+        }
+
+        for (int elem: month_31){ // Pour les mois de 31 jours
+            if(elem==this.month){
+                if (this.day==1){
+                    for (int elem2: month_31){
+                        if(elem2==this.month-1 || this.month==1){
+
+                            if (this.month==1){
+                                return new Date(31, 12, this.year-1);
+                            }
+                            else{
+                                return new Date(31, this.month-1, this.year);
+                            }
+                        }
+                    }
+                    if (this.month-1==2){
+                        if (this.year%4 ==0){
+                            return new Date(29, this.month-1, this.year);
+                        }
+                        else{
+                            return new Date(28, this.month-1, this.year);
+                        }
+                    }
+                    else{
+                        return new Date(30, this.month-1, this.year);
+                    }
+                    
+                }
+                else{
+                    return new Date(this.day-1, this.month, this.year);
+                }
+            }
+        }
+
+        if (this.day==1){ //Cas du premier Février
+            return new Date(31, this.month-1, this.year); 
+        }
+        else{ // Cas pour les autres jours de février
+            return new Date(this.day-1, this.month, this.year); 
+        }
+    }
+    @Override
+    public int compareTo(Date other) {
+        if (other==null){
+            throw new NullPointerException("Objet null");
+        }
+
+        else if (this.year==other.year && this.month==other.month && this.day==other.day){
+            return 0;
+        }
+
+        else if( this.year > other.year){
+            return 1;
+        }
+
+        else if( this.year < other.year){
+            return -1;
+        }
+
+        else if( this.month > other.month){
+            return 1;
+        }
+
+        else if( this.month < other.month){
+            return -1;
+        }
+
+        else if( this.day > other.day){
+            return 1;
+        }
+
+
+        else{
+            return -1;
+        }
+    }
+
+    public int getDay(){
+        return this.day;
+    }
+
+    public int getMonth(){
+        return this.month;
+    }
+
+    
+    public int getYear(){
+        return this.year;
+    }
 
 }
--- code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
+++ code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
@@ -1,10 +1,221 @@
 package fr.istic.vv;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DateTest {
+    // public static void main(String[] args) {
+    //     Date d1 = new Date(28, 2, 2012);
+    //     Date d2 = d1.nextDate();
+    //     System.out.println("d1 : "+d1.getDay()+"/"+d1.getMonth()+ "/"+d1.getYear());
+    //     System.out.println("d2 : "+d2.getDay()+"/"+d2.getMonth()+ "/"+d2.getYear());
+    //     System.out.println(d1.compareTo(d2));
+    // }
 
+    @Test
+    public void testConstructorValidDate() {
+        Date date = new Date(15, 8, 2021);
+        assertEquals(15, date.getDay());
+        assertEquals(8, date.getMonth());
+        assertEquals(2021, date.getYear());
+    }
+
+    @Test
+    public void testConstructorInvalidNumber() {
+        assertThrows(IllegalArgumentException.class, () -> {new Date(-1, 5, 2021);});
+        assertThrows(IllegalArgumentException.class, () -> {new Date(5, 0, 2021);});
+        assertThrows(IllegalArgumentException.class, () -> {new Date(32, 1, 2021);});
+        assertThrows(IllegalArgumentException.class, () -> {new Date(1, 13, 2021);});
+        assertThrows(IllegalArgumentException.class, () -> {new Date(31, 4, 2021);});
+    }
+
+
+
+    @Test
+    public void testConstructorFevrier() {
+        Date date = new Date(29, 2, 2020); // 2020 est une année bissextile
+        assertEquals(29, date.getDay());
+        assertEquals(2, date.getMonth());
+        assertEquals(2020, date.getYear());
+        assertThrows(IllegalArgumentException.class, () -> {new Date(29, 2, 2021);});
+        assertThrows(IllegalArgumentException.class, () -> {new Date(30, 2, 2020);});
+    }
+
+    @Test
+    public void testIsLeapYear(){
+        Date date1 = new Date(1, 1, 2024);
+        Date date2 = new Date(1, 1, 2023);
+        assertTrue(Date.isLeapYear(date1.getYear()));
+        assertFalse(Date.isLeapYear(date2.getYear()));
+    }
+
+    @Test
+    public void testNextDate() {
+        Date date1 = new Date(28, 2, 2021); 
+        Date nextDate1 = date1.nextDate();
+        assertEquals(1, nextDate1.getDay());
+        assertEquals(3, nextDate1.getMonth());
+        assertEquals(2021, nextDate1.getYear());
+
+        Date date2 = new Date(29, 2, 2020); 
+        Date nextDate2 = date2.nextDate();
+        assertEquals(1, nextDate2.getDay());
+        assertEquals(3, nextDate2.getMonth());
+        assertEquals(2020, nextDate2.getYear());
+
+        Date date3 = new Date(31, 12, 2021); 
+        Date nextDate3 = date3.nextDate();
+        assertEquals(1, nextDate3.getDay());
+        assertEquals(1, nextDate3.getMonth());
+        assertEquals(2022, nextDate3.getYear());
+
+        Date date4 = new Date(30, 7, 2024); 
+        Date nextDate4 = date4.nextDate();
+        assertEquals(31, nextDate4.getDay());
+        assertEquals(7, nextDate4.getMonth());
+        assertEquals(2024, nextDate4.getYear());
+    }
+
+    @Test
+    public void testPreviousDate() {
+        Date date1 = new Date(1, 3, 2021);
+        Date previousDate1 = date1.previousDate();
+        assertEquals(28, previousDate1.getDay());
+        assertEquals(2, previousDate1.getMonth());
+        assertEquals(2021, previousDate1.getYear());
+
+        Date date2 = new Date(1, 3, 2020);
+        Date previousDate2 = date2.previousDate();
+        assertEquals(29, previousDate2.getDay());
+        assertEquals(2, previousDate2.getMonth());
+        assertEquals(2020, previousDate2.getYear());
+
+        Date date3 = new Date(1, 1, 2022);
+        Date previousDate3 = date3.previousDate();
+        assertEquals(31, previousDate3.getDay());
+        assertEquals(12, previousDate3.getMonth());
+        assertEquals(2021, previousDate3.getYear());
+
+        Date date4 = new Date(1, 8, 2024);
+        Date previousDate4 = date4.previousDate();
+        assertEquals(31, previousDate4.getDay());
+        assertEquals(7, previousDate4.getMonth());
+        assertEquals(2024, previousDate4.getYear());
+    }
+
+    @Test
+    public void testCompareTo() {
+        Date date1 = new Date(5, 5, 2022);
+        Date date2 = new Date(4, 5, 2022);
+        assertEquals(1, date1.compareTo(date2));
+
+        date1 = new Date(4, 5, 2022);
+        date2 = new Date(4, 5, 2022);
+        assertEquals(0, date1.compareTo(date2));
+
+        date1 = new Date(3, 5, 2022);
+        date2 = new Date(4, 5, 2022);
+        assertEquals(-1, date1.compareTo(date2));
+    }
+
+    @Test
+    public void testPourAmeliorerLaCouverture(){
+        Date date1 = new Date(30, 4, 2022);
+        Date nextDate1 = date1.nextDate();
+        assertEquals(1, nextDate1.getDay());
+        assertEquals(5, nextDate1.getMonth());
+        assertEquals(2022, nextDate1.getYear());
+
+        date1 = new Date(15, 4, 2022);
+        nextDate1 = date1.nextDate();
+        assertEquals(16, nextDate1.getDay());
+        assertEquals(4, nextDate1.getMonth());
+        assertEquals(2022, nextDate1.getYear());
+
+        date1 = new Date(31, 10, 2021); 
+        nextDate1 = date1.nextDate();
+        assertEquals(1, nextDate1.getDay());
+        assertEquals(11, nextDate1.getMonth());
+        assertEquals(2021, nextDate1.getYear());
+
+        date1 = new Date(13, 2, 2020); 
+        nextDate1 = date1.nextDate();
+        assertEquals(14, nextDate1.getDay());
+        assertEquals(2, nextDate1.getMonth());
+        assertEquals(2020, nextDate1.getYear());
+
+        
+        date1 = new Date(13, 2, 2021); 
+        nextDate1 = date1.nextDate();
+        assertEquals(14, nextDate1.getDay());
+        assertEquals(2, nextDate1.getMonth());
+        assertEquals(2021, nextDate1.getYear());
+
+
+        Date date2 = new Date(1, 6, 2020);
+        Date previousDate2 = date2.previousDate();
+        assertEquals(31, previousDate2.getDay());
+        assertEquals(5, previousDate2.getMonth());
+        assertEquals(2020, previousDate2.getYear());
+
+        date2 = new Date(15, 6, 2020);
+        previousDate2 = date2.previousDate();
+        assertEquals(14, previousDate2.getDay());
+        assertEquals(6, previousDate2.getMonth());
+        assertEquals(2020, previousDate2.getYear());
+
+        date2 = new Date(1, 7, 2020);
+        previousDate2 = date2.previousDate();
+        assertEquals(30, previousDate2.getDay());
+        assertEquals(6, previousDate2.getMonth());
+        assertEquals(2020, previousDate2.getYear());
+
+        date2 = new Date(15, 7, 2020);
+        previousDate2 = date2.previousDate();
+        assertEquals(14, previousDate2.getDay());
+        assertEquals(7, previousDate2.getMonth());
+        assertEquals(2020, previousDate2.getYear());
+
+        date2 = new Date(1, 2, 2020);
+        previousDate2 = date2.previousDate();
+        assertEquals(31, previousDate2.getDay());
+        assertEquals(1, previousDate2.getMonth());
+        assertEquals(2020, previousDate2.getYear());
+
+        date2 = new Date(8, 2, 2020);
+        previousDate2 = date2.previousDate();
+        assertEquals(7, previousDate2.getDay());
+        assertEquals(2, previousDate2.getMonth());
+        assertEquals(2020, previousDate2.getYear());
+
+        
+        final Date date99 = new Date(1, 1, 2022);
+        assertThrows(NullPointerException.class, () -> date99.compareTo(null));
+        
+        date1 = new Date(2, 2, 2022);
+        date2 = new Date(2, 2, 2022);
+        assertEquals(0, date1.compareTo(date2));
+
+        Date date3 = new Date(2, 2, 2023);
+        assertEquals(1, date3.compareTo(date2));
+
+        Date date4 = new Date(2, 2, 2021);
+        assertEquals(-1, date4.compareTo(date2));
+
+        Date date5 = new Date(1, 3, 2022);
+        assertEquals(1, date5.compareTo(date2));
+
+        Date date6 = new Date(2, 1, 2022);
+        assertEquals(-1, date6.compareTo(date2));
+
+        Date date7 = new Date(3, 2, 2022);
+        assertEquals(1, date7.compareTo(date2));
+
+        Date date8 = new Date(1, 2, 2022);
+        assertEquals(-1, date8.compareTo(date2));
+
+    }
 
 }
